6d45ea22fe5189ae82b02eef5f614efe8647b925
emuguy1 <emanuel.erben@gmail.com>
2017-06-28 18:01:40 +0200

    buttons invisible

--- Rouletttisch_v0.3/Spielbrett.java
+++ Rouletttisch_v0.3/Spielbrett.java
@@ -1,4 +1,6 @@
+import java.awt.Dimension;
 import javax.swing.*;
+import java.awt.Color;
 public class Spielbrett extends JFrame implements View{
     /**
 	 * The WIDTH of the JFrame:
@@ -8,7 +10,8 @@ public class Spielbrett extends JFrame implements View{
 	 * The HEIGHT of the JFrame:
 	 */
 	private final int HEIGHT = 660;
-	
+	private JButton b1 = new JButton("hi");
+	private JButton b2 = new JButton("2");
 	//Controller:
 	private Controller controller;
 	private JMenuBar menuBar = new JMenuBar();;
@@ -21,11 +24,16 @@ public class Spielbrett extends JFrame implements View{
 		setSize(WIDTH, HEIGHT);
 		setLocationRelativeTo(null);
 		setResizable(false);
-		
-		
-		
 		//Hintergrundblid setzen
 		setContentPane(new JLabel(new ImageIcon(System.getProperty("user.dir")+"/roulette.jpg")));
+		//adding the ActionListener
+		b1.addActionListener(controller);
+		b2.addActionListener(controller);
+		//adding components
+		addB1();
+		addB2();
+		
+		
 		menuBar.add(menu);
 		add(menuBar);
 		
@@ -37,6 +45,16 @@ public class Spielbrett extends JFrame implements View{
         controller.setView(this);
     }
     
-    
+    private void addB1(){
+		add(b1);
+		b1.setBounds(300,800,30,30);
+		//b1.setBackground(new Color(0,0,0,0));
+		//b1.setBorder(null);
+		//b1.setContentAreaFilled(false);
+    }
+	private void addB2(){
+		add(b2);
+		b2.setBounds(80,20,40,40);
+	}
     
 }
