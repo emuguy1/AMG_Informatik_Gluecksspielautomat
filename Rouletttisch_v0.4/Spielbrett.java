850751a269fe5b360d37c841584d5c1144e8d771
emuguy1 <emanuel.erben@gmail.com>
2017-07-09 19:57:23 +0200

    1

--- Rouletttisch_v0.4/Spielbrett.java
+++ Rouletttisch_v0.4/Spielbrett.java
@@ -134,7 +134,7 @@ public class Spielbrett extends JFrame implements View{
         b47.addActionListener(controller);
         b48.addActionListener(controller);
         b49.addActionListener(controller);
-        comboBox.setBounds(200,200,300,300);
+        comboBox.setBounds(100,200,300,300);
         add(comboBox);
         //adding components
         //addB4();
